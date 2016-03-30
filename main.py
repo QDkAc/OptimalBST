2ced79f8128e37fd15c243c1c40decd1794b8278
RuosongWang <wrs13@mails.tsinghua.edu.cn>
2016-03-30 15:29:58 -0400

    fix xrange

--- main.py
+++ main.py
@@ -4,14 +4,15 @@ import sequential
 import greedyASS
 import path
 import traversal
+import splay
 
 n = 10
 access_sequence = traversal.generate(n)
 #n = 10
 #access_sequence = [7, 0, 6, 3, 5, 2, 9, 8, 1, 4]
-algorithm = greedyASS.greedyASS(n)
+algorithm = splay.Splay([n])
 
-for i in xrange(len(access_sequence)):
+for i in range(len(access_sequence)):
 	touch_points = algorithm.access(access_sequence[i])
 	plt.plot(access_sequence[i], [i], marker = "o", color = "r")
 	for p in touch_points:
